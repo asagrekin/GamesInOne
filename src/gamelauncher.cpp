6a9ba45b7d1deae202ae326c04c5567191ef84de
Leonardo Ochoa <lochoa2@uw.edu>
2023-04-15 14:48:26 -0700

    user input methods

--- src/gamelauncher.cpp
+++ src/gamelauncher.cpp
@@ -1,14 +1,38 @@
 // Kelby S. & Leonardo O.
 // 4/11/2023
-#include<Windows.h>
-#include<iostream>
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fstream>
+#include <algorithm>
+
+
 using namespace std;
 
+BOOL checkPath(std::string path) ;
 
-int main()
-{
-    // Shell option
-    // ShellExecute(NULL, TEXT("open"), TEXT("C:\\Windows\\notepad.exe"), NULL, NULL, SW_SHOWDEFAULT);
+void EnhancePath(std::string& path) ;
+
+int main() {
+
+    std::string full_path;
+    std::cout << "Enter path: ";
+    std::getline(std::cin, full_path);
+
+    // make sure path is readable
+    EnhancePath(full_path);
+    if (full_path.length() == 0) {
+        return EXIT_FAILURE;
+    }
+
+    // check valid path
+    if (!checkPath(full_path)) {
+        cout << "The path is not valid!";
+        return EXIT_FAILURE;
+    }
+
 
     // Don't really need
     HANDLE hProcess;
@@ -22,7 +46,9 @@ int main()
     ZeroMemory(&startinfo, sizeof(startinfo));
     ZeroMemory(&processinfo, sizeof(processinfo));
 
-	BOOL bScucces = CreateProcess(TEXT("C:\\Windows\\notepad.exe"), NULL,
+    char* path = &full_path[0];
+
+	BOOL bScucces = CreateProcess(NULL, path,
 			NULL, NULL, FALSE, 0, NULL, NULL, &startinfo, &processinfo);
 
 
@@ -50,4 +76,33 @@ int main()
     // the game then this will resume the tasks in this main.
     system("PAUSE");
     return 0;
+}
+
+BOOL checkPath(std::string dir) {
+    std::ifstream file(dir);
+    if (file.good()) {
+        file.close();
+        return true;
+    } else {
+        file.close();
+        return false;
+    }
+}
+
+
+// Validates game is an exe file and fixes escape characters in the path.
+// Inputs: A string representing supposed game's path.
+// Outputs: A validated .exe string for the games with fixed exit characters.
+// EXIT_FAILURE: If the input string path is not a .exe file.
+void EnhancePath(std::string& path) {
+    path.erase(std::remove(path.begin(), path.end(), '\"'), path.end());
+    if (path.substr(path.length() - 4) == ".exe"){
+        size_t i = 0;
+        while ((i = path.find("\\", i)) != std::string::npos) {
+            path.replace(i, 1, "/");
+            i += 1;
+        }
+    } else {
+        path = "";
+    }
 }
