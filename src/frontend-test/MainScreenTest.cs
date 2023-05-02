6661d62444a73f247822afde247ef8f3bc61c497
Steven <slok@cs.washington.edu>
2023-05-02 14:39:49 -0700

    Added MSTest

---
+++ src/frontend-test/MainScreenTest.cs
@@ -0,0 +1,16 @@
+using Games_In_One_App;
+using System.Windows.Forms;
+
+namespace frontend_test
+{
+    [TestClass]
+    public class MainScreenTest
+    {
+        [TestMethod]
+        public void TestAddGameScreenShowing()
+        {
+            GamesInOne mainScreen = new GamesInOne();
+           
+        }
+    }
+}
