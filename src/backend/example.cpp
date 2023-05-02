3e4b106e8cf40372f5c188a50c66e551559e1b0c
Kelby Steinmetz <kelbys2@cs.washington.edu>
2023-05-02 13:35:46 -0700

    First Push of Tests

---
+++ src/backend/example.cpp
@@ -0,0 +1,5 @@
+#include "example.hpp"
+
+int MAC(int x, int y, int& sum){
+   return sum + x * y;
+}
---
+++ src/backend/example.hpp
@@ -0,0 +1,9 @@
+#ifndef EXAMPLE_HPP
+#define EXAMPLE_HPP
+
+
+int MAC(x,y,sum);
+
+
+
+#endif /* EXAMPLE_HPP */
