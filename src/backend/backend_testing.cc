d71618000f0ca5f862968c43b2155363eb0841ed
Kelby Steinmetz <kelbys2@cs.washington.edu>
2023-05-15 21:47:19 -0700

    Beta Released Testing Complete - All methods tested and functioning

--- src/backend/backend_testing.cc
+++ src/backend/backend_testing.cc
@@ -6,16 +6,20 @@
 using namespace std;
 
 
-// int main(int argc, char** argv) {
-//   testing::InitGoogleTest(&argc, argv);
-//   launcher::get_data();
-//   GTEST_FLAG_SET(ready_flag, "ready");
-//   return RUN_ALL_TESTS();
-// }
+//                             NOTES FOR TESTING
+// This application heavily relies on file paths from one's local computer. These tests
+// are catered to a windows device and are using pre-installed file paths such as
+// that of Windows calculator and notepad. If testing does not result in up-to-standard
+// results please refer to the following to fix any path issues that may arrise.
+//
+// The below executable and image paths will need to be change to rely on a local path.
+//          4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22
+//
+// Fixing these paths to match that of those in one's local files should fix any issues
+// that may arise when using the Google Test suite.
 
 
-
-//   File Name,    Test Name
+// TEST # 1 : Enhance path holds expected behavior for different file types.
 TEST(ExampleTests, EnhancePath_FileTypeTests) {
    // File Types
    string exe =  ".exe";
@@ -58,7 +62,7 @@ TEST(ExampleTests, EnhancePath_FileTypeTests) {
 }
 
 
-
+// TEST # 2 : Enhance path holds expected behavior when fixing a path's slashes.
 TEST(ExampleTests, EnhancePath_FixSlash) {
    // File Types
    string exe =  ".exe";
@@ -68,7 +72,7 @@ TEST(ExampleTests, EnhancePath_FixSlash) {
    string ico =  ".ico";
    string random = ".random";
 
-   // Paths - both test and fixed
+   // Paths - Testing
    string test = "test\\the\\path";
    string path1 = test + exe;
    string path2 = test + png;
@@ -77,6 +81,7 @@ TEST(ExampleTests, EnhancePath_FixSlash) {
    string path5 = test + ico;
    string path6 = test + random;
 
+   // Paths - Fixed
    string fixed = "test/the/path";
    string fixed1 = fixed + exe;
    string fixed2 = fixed + png;
@@ -101,7 +106,7 @@ TEST(ExampleTests, EnhancePath_FixSlash) {
 }
 
 
-
+// TEST # 3 : Enhance path handles bad inputs appropriately.
 TEST(ExampleTests, EnhancePath_BadInputs) {
    // Testing Strings
    string exe = ".exe";
@@ -125,9 +130,8 @@ TEST(ExampleTests, EnhancePath_BadInputs) {
 }
 
 
-
+// TEST # 4 : CheckPath correctly distinguishes valid paths.
 TEST(ExampleTests, checkpath) {
-   // "C:\Windows\System32\notepad.exe"
    string dir = "C:/Windows/System32/notepad.exe";
    string wrongdir = "C:WindowsSystem32otepad.png";
    string random = "\\random.exe";
@@ -138,7 +142,7 @@ TEST(ExampleTests, checkpath) {
 }
 
 
-
+// TEST # 5 : LaunchGame launches provided executables as intended.
 TEST(ExampleTests, LaunchGameGood) {
    string dir = "C:/Windows/System32/notepad.exe";
 
@@ -149,13 +153,12 @@ TEST(ExampleTests, LaunchGameGood) {
 
    // Compare the captured output with the expected output
    EXPECT_EQ(output, "Create Process Success\n");
-
 }
 
 
-
+// TEST # 6 : LaunchGame rejects invalid executables as intended.
 TEST(ExampleTests, LaunchGameBad) {
-   // Missing slashes so it wont launch
+   // Missing slashes in path so it should not launch
    string dir = "C:WindowsSystem32notepad.exe";
 
    // Capture the standard output produced by MyMethod
@@ -165,42 +168,144 @@ TEST(ExampleTests, LaunchGameBad) {
 
    // Compare the captured output with the expected output
    EXPECT_EQ(output, "Create Process Failed\n");
-
 }
 
 
+// TEST # 7 : AddGame promptly interacts with the database to add a game.
 TEST(ExampleTests, AddGame) {
-   //GTEST_FLAG_SET(ready_flag, "ready");
-
-   // string add(string game_name, string game_path, string image_path
    string game_name = "notepad";
    string game_path = "C:/Windows/System32/notepad.exe";
    string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
-   //src\backend\uw.jpg
 
-   // list<gamesDB::dbObject*>* gamelist = launcher::get_data();
-   // for (list<gamesDB::dbObject*>::iterator it = gamelist->begin(); it != gamelist->end(); it++) {
-   //    delete (*it);
-   // }
-   // delete gamelist;
-
-
+   // Clear database and get the data before beginning.
    gamesDB::clearDB();
    launcher::get_data();
+   // Add the game to the database and collect output message.
    string output = launcher::add(game_name, game_path, image_path);
    EXPECT_EQ(output, "success");
 }
 
 
+// TEST # 8 : AddGame rejects the addition due to bad game path.
+TEST(ExampleTests, AddGameBadGamePath) {
+   // Game path is now just lolz.
+   string game_name = "notepad";
+   string game_path = "lolz";
+   string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
+
+   // Clear database and get the data before beginning.
+   gamesDB::clearDB();
+   launcher::get_data();
+   // Add the game to the database and collect output message.
+   string output = launcher::add(game_name, game_path, image_path);
+   EXPECT_EQ(output, "Not a valid .exe file!");
+}
+
+
+// TEST # 9 : AddGame rejects the addition due to invalid game path.
+TEST(ExampleTests, AddGameInvalidGamePath) {
+   // Game path is now not a valid local path.
+   string game_name = "notepad";
+   string game_path = "C:/Testing/Is/Not/Fun.exe";
+   string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
+
+   // Clear database and get the data before beginning.
+   gamesDB::clearDB();
+   launcher::get_data();
+   // Add the game to the database and collect output message.
+   string output = launcher::add(game_name, game_path, image_path);
+   EXPECT_EQ(output, "The game path is not valid!");
+}
+
+
+// TEST # 10 : AddGame rejects the addition due to bad image path.
+TEST(ExampleTests, AddGameBadImagePath) {
+   // Image path is now just lolz.
+   string game_name = "notepad";
+   string game_path = "C:/Windows/System32/notepad.exe";
+   string image_path = "lolz";
+
+   // Clear database and get the data before beginning.
+   gamesDB::clearDB();
+   launcher::get_data();
+   // Add the game to the database and collect output message.
+   string output = launcher::add(game_name, game_path, image_path);
+   EXPECT_EQ(output, "Not a valid .ico, png, or jpeg/jpg file!");
+}
+
+
+// TEST # 11 : AddGame rejects the addition due to invalid image path.
+TEST(ExampleTests, AddGameInvalidImagePath) {
+   // Image path is now not a valid local path.
+   string game_name = "notepad";
+   string game_path = "C:/Windows/System32/notepad.exe";
+   string image_path = "C:/Testing/Is/Not/Fun.jpg";
+
+   // Clear database and get the data before beginning.
+   gamesDB::clearDB();
+   launcher::get_data();
+   // Add the game to the database and collect output message.
+   string output = launcher::add(game_name, game_path, image_path);
+   EXPECT_EQ(output, "The image path is not valid!");
+}
+
+
+// TEST # 12 : AddGame rejects the new add because it already exists in the database.
+TEST(ExampleTests, AddGameAlreadyExists) {
+   string game_name = "notepad";
+   string game_path = "C:/Windows/System32/notepad.exe";
+   string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
+
+   // Clear database and get the data before beginning.
+   gamesDB::clearDB();
+   launcher::get_data();
+   // Add the game to the database and collect output message.
+   launcher::add(game_name, game_path, image_path);
+   string output = launcher::add(game_name, game_path, image_path);
+   EXPECT_EQ(output, "this path already exists!");
+}
+
+
+// TEST # 13 : Delete Game succesfully deletes a game from the database.
 TEST(ExampleTests, DeleteGame) {
    string game_name = "notepad";
    string game_path = "C:/Windows/System32/notepad.exe";
    string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
+
+   // Add a game before deleting
+   gamesDB::clearDB();
+   list<gamesDB::dbObject*>* games = launcher::get_data();
+   launcher::add(game_name, game_path, image_path);
+
+   // Aquire the id to use for the delete
+   games = launcher::get_data();
+   int id;
+   list<gamesDB::dbObject*>::iterator it;
+   for (it = games->begin(); it != games->end(); it++) {
+      string game = (*it)->getName();
+      if (game == "notepad") {
+         id = (*it)->getID();
+      }
+   }
+
+   // Delete the game with the selected id.
+   string del_result = launcher::del(id);
+   EXPECT_EQ(del_result, "success");
+}
+
+
+// TEST # 14 : Delete Game was not successful because the game was already deleted.
+TEST(ExampleTests, DeleteGameBad) {
+   string game_name = "notepad";
+   string game_path = "C:/Windows/System32/notepad.exe";
+   string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
+
    // Add a game
    gamesDB::clearDB();
    list<gamesDB::dbObject*>* games = launcher::get_data();
    launcher::add(game_name, game_path, image_path);
 
+   // Aquire the id to use for the delete
    games = launcher::get_data();
    int id;
    list<gamesDB::dbObject*>::iterator it;
@@ -211,32 +316,45 @@ TEST(ExampleTests, DeleteGame) {
       }
    }
 
-    string del_result = launcher::del(id);
-   EXPECT_EQ(del_result, "success");
+   // Delete the game with the selected id twice to invoke error.
+   launcher::del(id);
+   string del_result = launcher::del(id);
+   EXPECT_EQ(del_result, "game could not be deleted");
 }
 
 
+// TEST # 15 : Game List successfully lists added games.
 TEST(ExampleTests, GameList) {
    string game_name = "notepad";
    string game_path = "C:/Windows/System32/notepad.exe";
    string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
 
+   string game_name_2 = "calculator";
+   string game_path_2 = "C:/Windows/System32/calc.exe";
 
+   // Add two games to use for the gamelist.
    gamesDB::clearDB();
    list<gamesDB::dbObject*>* games = launcher::get_data();
    launcher::add(game_name, game_path, image_path);
+   launcher::add(game_name_2, game_path_2, image_path);
 
+   // Get both game's id values from the database.
    games = launcher::get_data();
-   int id;
+   int id, id2;
    list<gamesDB::dbObject*>::iterator it;
    for (it = games->begin(); it != games->end(); it++) {
       string game = (*it)->getName();
       if (game == "notepad") {
          id = (*it)->getID();
       }
+      if (game == "calculator") {
+         id2 = (*it)->getID();
+      }
    }
-   string expected = "Name: notepad,  ID: " + to_string(id) + "\n";
-   cout<<expected<<endl;
+
+   // Build our expected and invoke the displayed behavior from calling the method.
+   string expected = "Name: notepad,  ID: " + to_string(id) + "\n" +
+                     "Name: calculator,  ID: " + to_string(id2) + "\n";
    testing::internal::CaptureStdout();
    launcher::gameList();
    std::string output = testing::internal::GetCapturedStdout();
@@ -245,15 +363,75 @@ TEST(ExampleTests, GameList) {
 }
 
 
-TEST(ExampleTests, Play) {
+// TEST # 16 : Game List successfully lists no games when the database is empty.
+TEST(ExampleTests, GameListEmpty) {
+   // Set up an empty games list.
+   gamesDB::clearDB();
+   list<gamesDB::dbObject*>* games = launcher::get_data();
+
+   // Save the invoked behavior from calling gameList.
+   testing::internal::CaptureStdout();
+   launcher::gameList();
+   std::string output = testing::internal::GetCapturedStdout();
+
+   EXPECT_EQ(output, "");
+}
+
+
+// TEST # 17 : Game List successfully lists appropriate games after adds and deletes occured.
+TEST(ExampleTests, GameListAddDeleteGame) {
    string game_name = "notepad";
    string game_path = "C:/Windows/System32/notepad.exe";
    string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
 
+   string game_name_2 = "calculator";
+   string game_path_2 = "C:/Windows/System32/calc.exe";
+
+   // Add two games to use for the gamelist.
+   gamesDB::clearDB();
+   list<gamesDB::dbObject*>* games = launcher::get_data();
+   launcher::add(game_name, game_path, image_path);
+   launcher::add(game_name_2, game_path_2, image_path);
+
+   // Get both game's id values from the database.
+   games = launcher::get_data();
+   int id, id2;
+   list<gamesDB::dbObject*>::iterator it;
+   for (it = games->begin(); it != games->end(); it++) {
+      string game = (*it)->getName();
+      if (game == "notepad") {
+         id = (*it)->getID();
+      }
+      if (game == "calculator") {
+         id2 = (*it)->getID();
+      }
+   }
+
+   // Delete the first game to invoke the database reaction.
+   launcher::del(id);
+
+   // Build our expected and invoke the displayed behavior from calling the method.
+   string expected = "Name: calculator,  ID: " + to_string(id2) + "\n";
+   testing::internal::CaptureStdout();
+   launcher::gameList();
+   std::string output = testing::internal::GetCapturedStdout();
+
+   EXPECT_EQ(output, expected);
+}
+
+
+// TEST # 18 : Play executes the selected game from the database as expected.
+TEST(ExampleTests, PlayGood) {
+   string game_name = "notepad";
+   string game_path = "C:/Windows/System32/notepad.exe";
+   string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
+
+   // Setup and add a game to the database.
    gamesDB::clearDB();
    list<gamesDB::dbObject*>* games = launcher::get_data();
    launcher::add(game_name, game_path, image_path);
 
+   // Get the id for the game added to the database.
    games = launcher::get_data();
    int id;
    list<gamesDB::dbObject*>::iterator it;
@@ -264,15 +442,38 @@ TEST(ExampleTests, Play) {
       }
    }
 
+   // Collect the status of the play method and if true the play was a success.
    boolean output = launcher::play(id);
-   EXPECT_EQ(output, true);
+   EXPECT_TRUE(output);
 }
 
+
+// TEST # 19 : Play rejects the selected game as it does not exist in the database.
+TEST(ExampleTests, PlayBad) {
+   string game_name = "notepad";
+   string game_path = "C:/Windows/System32/notepad.exe";
+   string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
+   int random_id = 123456;
+
+   // Set up database and add a game to it.
+   gamesDB::clearDB();
+   list<gamesDB::dbObject*>* games = launcher::get_data();
+   launcher::add(game_name, game_path, image_path);
+   launcher::get_data();
+
+   // Try launching an id that does not exist in the database.
+   boolean output = launcher::play(random_id);
+   EXPECT_FALSE(output);
+}
+
+
+// TEST # 20 : The call to get_data is succesful in intializing the database.
 TEST(ExampleTests, GetData) {
    string game_name = "notepad";
    string game_path = "C:/Windows/System32/notepad.exe";
    string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
 
+   // Set up the database and invoke succeed if the call was a success.
    gamesDB::clearDB();
    list<gamesDB::dbObject*>* games = launcher::get_data();
    launcher::add(game_name, game_path, image_path);
@@ -281,17 +482,36 @@ TEST(ExampleTests, GetData) {
 }
 
 
-TEST(ExampleTests, HasPath) {
+// TEST # 21 : HasPath successfully finds the given game_path in the database.
+TEST(ExampleTests, HasPathGood) {
    string game_name = "notepad";
    string game_path = "C:/Windows/System32/notepad.exe";
    string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
 
+   // Setup database and add a game to it.
    gamesDB::clearDB();
    launcher::get_data();
    launcher::add(game_name, game_path, image_path);
 
+   // After adding game check that the database has the path for this game saved.
    boolean output = launcher::hasPath(game_path);
-   cout<<output<<endl;
    EXPECT_TRUE(output);
+}
 
+
+// TEST # 22 : HasPath unsuccessfully finds the given game_path in the database.
+TEST(ExampleTests, HasPathBad) {
+   string game_name = "notepad";
+   string game_path = "C:/Windows/System32/notepad.exe";
+   string image_path = "C:/Users/kelby/OneDrive/Desktop/uw.jpg";
+   string bad_name = "chrome";
+
+   // Setup database and add a game to it.
+   gamesDB::clearDB();
+   launcher::get_data();
+   launcher::add(game_name, game_path, image_path);
+
+   // Check if the database has a path that does not exist to invoke bad behavior.
+   boolean output = launcher::hasPath(bad_name);
+   EXPECT_FALSE(output);
 }
